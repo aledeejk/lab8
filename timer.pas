e4c7b5052d73f39336868876b4d71e75a70b594a
Деева Александра <aledee@sfedu.ru>
2023-09-30 11:17:05 +0300

    Написала программу, которая считает секунды из написанных данных(часы, минуты и секунды) пользоваьеля.

---
+++ timer.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var chas := readinteger('Введите сколько часов:');
+  var minut := readinteger('Введите сколько минут:');
+  var sek := readinteger('Введите сколько часов:');
+  chas *= 3600;
+  minut *= 60;
+  sek += chas + minut;
+  print (sek, 'секунд');
+end.
