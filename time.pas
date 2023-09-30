57fbe5bdcae97ab12c686a29a2b6fb8d48568120
Деева Александра <aledee@sfedu.ru>
2023-09-30 10:55:20 +0300

    Написала программу, которая подсчитывает высокосный год или нет

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var god := readinteger('Введите год:');
+  if (god mod 4 = 0) or (god mod 100 = 0) and (god mod 400 <>0) then
+    print ('Этот год высокосный')
+  else
+    print('Этот год не высокосный');
+end.
