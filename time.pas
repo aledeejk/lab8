9f297c349376fb9aaf0988afc4192fc3c40f68d9
Деева Александра <aledee@sfedu.ru>
2023-09-30 11:07:02 +0300

    Изменила программу, которая говорит сколько дней в году.

--- time.pas
+++ time.pas
@@ -1,7 +1,7 @@
 ﻿begin
   var god := readinteger('Введите год:');
   if (god mod 4 = 0) or (god mod 100 = 0) and (god mod 400 <>0) then
-    print ('Этот год высокосный')
+    print ('366 дней.')
   else
-    print('Этот год не высокосный');
+    print('365 дней.');
 end.
