e8dedb50e710e46f5f5d190b6012642c62605380
Деева Александра <aledee@sfedu.ru>
2023-09-30 10:45:42 +0300

    Написала программу, выводящую некоторое соообщение на час, который указал пользователь

---
+++ hello.pas
@@ -0,0 +1,12 @@
+﻿begin
+  var chas := readinteger('Который час?');
+  assert((chas>=0) and (chas<=23));
+  var s := '';
+  case chas of
+    4..10: s := 'Доброе утро, мир!';
+    11..16: s := 'Добрый день, мир!';
+    17..22: s := 'Добрый вечер, мир!';
+    else s := 'Доброй ночи, мир!';
+  end;
+print(s);
+end.
